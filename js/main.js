37254f2472bfde387004e105f0e789f5528d8719
Javier Forero <javierforero01@gmail.com>
2017-10-17 10:32:24 -0500

    first commit of swiperdemo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#ad-drawer-arrow').click(function(){
+
+    $('#ad-drawer').toggleClass('drawer-animation');
+    $('#ad-drawer-arrow').toggleClass('drawer-arrow-animation');
+
+  });
+});
